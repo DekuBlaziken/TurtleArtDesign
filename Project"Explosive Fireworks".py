ec13fa4068be41bcb2234dee1879f29534b07021
DekuBlaziken <33318941+DekuBlaziken@users.noreply.github.com>
2017-11-06 08:17:34 -0500

    Code For Project Design
    Its The Code For The Project "Explosive Fireworks"

---
+++ Project"Explosive Fireworks".py
@@ -0,0 +1,145 @@
+import turtle
+turtle.colormode(255)
+turtle.bgcolor("black")
+
+bob=turtle.Turtle()
+bob.speed(0)
+turtle.tracer(0)
+
+for times in range(375):
+    bob.circle(160)
+    bob.left(5)
+    bob.color(255,255,255)
+
+
+for times in range(375):
+    bob.circle(180)
+    bob.left(5)
+    bob.color(0,255,255)
+    
+
+for times in range(375):
+    bob.circle(200)
+    bob.left(5)
+    bob.color(255,0,0)    
+
+for times in range(375):
+    bob.circle(220)
+    bob.left(5)
+    bob.color(150,0,150)    
+
+for times in range(375):
+    bob.circle(240)
+    bob.left(5)
+    bob.color(0,0,255)    
+
+for times in range(375):
+    bob.circle(260)
+    bob.left(5)
+    bob.color(150,0,0)    
+
+for times in range(60):
+    bob.penup()
+    bob.goto(0,0)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(241)
+    bob.color(0,255,255)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(600,300)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(255,255,255)
+    
+for times in range(100):
+    bob.penup()
+    bob.goto(650,100)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(0,255,255)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(675,-100)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(0,0,255)
+    
+for times in range(100):
+    bob.penup()
+    bob.goto(600,-300)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(255,0,0)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(-600,-300)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(100,200,150)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(-650,-100)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(200,150,100)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(-625,120)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(45,87,93)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(-550,320)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(91)
+    bob.color(100,123,168)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(0,0)
+    bob.pendown()
+    bob.forward(150)
+    bob.left(91)
+    bob.color(0,0,255)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(0,0)
+    bob.pendown()
+    bob.forward(200)
+    bob.left(91)
+    bob.color(255,0,0)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(0,0)
+    bob.pendown()
+    bob.forward(250)
+    bob.left(91)
+    bob.color(0,0,255)
+
+for times in range(100):
+    bob.penup()
+    bob.goto(0,0)
+    bob.pendown()
+    bob.forward(300)
+    bob.left(91)
+    bob.color(150,160,175)
+
+
